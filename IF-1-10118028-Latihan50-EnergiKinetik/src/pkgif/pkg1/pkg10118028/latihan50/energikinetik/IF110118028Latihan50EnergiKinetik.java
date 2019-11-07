d6066fa5e92ea07a00d70e3ea5af7938030fdeaf
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-11-07 22:51:05 +0700

    Add files via upload

---
+++ IF-1-10118028-Latihan50-EnergiKinetik/src/pkgif/pkg1/pkg10118028/latihan50/energikinetik/IF110118028Latihan50EnergiKinetik.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg1.pkg10118028.latihan50.energikinetik;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Energi Kinetik
+
+ */
+public class IF110118028Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EnergiKinetik ek = new EnergiKinetik();
+        ek.tampilHasil();
+    }
+    
+}
